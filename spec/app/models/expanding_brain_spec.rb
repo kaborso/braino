8172fd1e43d9d03e93f8d1e921c624cd338451fb
Akil Brooks <akil.brooks@gmail.com>
2017-05-11 17:24:25 -0400

    work on EB model tests

--- spec/app/models/expanding_brain_spec.rb
+++ spec/app/models/expanding_brain_spec.rb
@@ -14,15 +14,13 @@ describe 'expanding brain model' do
       it 'uploads image' do
         eb = ExpandingBrain.new(brains)
 
-        # allow_any_instance_of(Brain).to receive(:render).and_return("done")
-        allow(eb).to receive_message_chain(:first, :render).and_return("done")
-        allow(eb).to receive_message_chain(:second, :render).and_return("done")
-        allow(eb).to receive_message_chain(:third, :render).and_return("done")
-        allow(eb).to receive_message_chain(:fourth, :render).and_return("done")
+        allow_any_instance_of(Brain).to receive(:render).and_return("done")
+        allow(File).to receive(:exist?).and_return(true)
         allow(eb).to receive(:track).with("image.generate.timer", instance_of(Fixnum)).and_return(true)
-        expect(Storage).to receive(:upload_image).and_return(true)
+        allow(Storage).to receive(:upload_image).and_return(true)
         allow(eb).to receive(:track).with("image.upload.timer", instance_of(Fixnum)).and_return(true)
-        allow(Storage).to receive(:get_image_url).and_return(true)
+        allow(Storage).to receive(:get_image_url).and_return('https://expanding-brain.s3.us-east-2.amazonaws.com/someimage.png')
+
         expect(eb.generate).to be_a_kind_of(String)
       end
 
@@ -30,39 +28,48 @@ describe 'expanding brain model' do
         eb = ExpandingBrain.new(brains)
 
         allow_any_instance_of(Brain).to receive(:render).and_return("done")
+        allow(File).to receive(:exist?).and_return(true)
         allow(eb).to receive(:track).with("image.generate.timer", instance_of(Fixnum)).and_return(true)
-        expect(Storage).to receive(:upload_image).and_raise(StandardError)
+        allow(Storage).to receive(:upload_image).and_raise(StandardError, "It should handle this")
         allow(eb).to receive(:track).with("image.upload.timer", instance_of(Fixnum)).and_return(true)
-        allow(Storage).to receive(:get_image_url).and_return(true)
-        expect(eb).to receive(:track).with("error", instance_of(Fixnum)).and_return(true)
-        expect(eb.generate).to be_a_kind_of(String)
+        allow(Storage).to receive(:get_image_url).and_return("done")
+        allow(eb).to receive(:track).with("error", instance_of(Fixnum)).and_return(true)
+
+        expect{ eb.generate }.to raise_error(ExpandingBrainError)
       end
 
       it 'retrieves presigned url' do
         eb = ExpandingBrain.new(brains)
 
         allow_any_instance_of(Brain).to receive(:render).and_return("done")
+        allow(File).to receive(:exist?).and_return(true)
         allow(eb).to receive(:track).with("image.generate.timer", instance_of(Fixnum)).and_return(true)
         allow(Storage).to receive(:upload_image).and_return(true)
         allow(eb).to receive(:track).with("image.upload.timer", instance_of(Fixnum)).and_return(true)
-        expect(Storage).to receive(:get_image_url).and_return('https://expanding-brain.s3.us-east-2.amazonaws.com/someimage.png')
+        allow(Storage).to receive(:get_image_url).and_return('https://expanding-brain.s3.us-east-2.amazonaws.com/someimage.png')
+
         expect(eb.generate).to be_a_kind_of(String)
       end
 
       it 'recovers from errors while fetching url' do
         eb = ExpandingBrain.new(brains)
 
+        allow_any_instance_of(Brain).to receive(:render).and_return("done")
+        allow(File).to receive(:exist?).and_return(true)
         allow(eb).to receive(:track).with("image.generate.timer", instance_of(Fixnum)).and_return(true)
         allow(Storage).to receive(:upload_image).and_return(true)
         allow(eb).to receive(:track).with("image.upload.timer", instance_of(Fixnum)).and_return(true)
-        expect(Storage).to receive(:get_image_url).and_raise(StandardError)
-        expect(eb).to receive(:track).with("error", instance_of(Fixnum)).and_return(true)
-        expect(eb.generate).to be_a_kind_of(String)
+        allow(Storage).to receive(:get_image_url).and_raise(StandardError, "It should handle this")
+        allow(eb).to receive(:track).with("error", instance_of(Fixnum)).and_return(true)
+
+        expect{ eb.generate }.to raise_error(ExpandingBrainError)
       end
 
       it 'returns an AWS url' do
         eb = ExpandingBrain.new(brains)
 
+        allow_any_instance_of(Brain).to receive(:render).and_return("done")
+        allow(File).to receive(:exist?).and_return(true)
         allow(eb).to receive(:track).with("image.generate.timer", instance_of(Fixnum)).and_return(true)
         allow(Storage).to receive(:upload_image).and_return(true)
         allow(eb).to receive(:track).with("image.upload.timer", instance_of(Fixnum)).and_return(true)
@@ -76,17 +83,19 @@ describe 'expanding brain model' do
   end
 
   describe 'static' do
-    it '#generate returns an AWS url' do
+    it '#generate returns an instance with a named and uploaded image' do
       allow_any_instance_of(Brain).to receive(:render).and_return("done")
-
+      allow(File).to receive(:exist?).and_return(true)
       allow_any_instance_of(ExpandingBrain).to receive(:track).with("image.generate.timer", instance_of(Fixnum)).and_return(true)
       allow(Storage).to receive(:upload_image).and_return(true)
       allow_any_instance_of(ExpandingBrain).to receive(:track).with("image.upload.timer", instance_of(Fixnum)).and_return(true)
       allow(Storage).to receive(:get_image_url).and_return("https://expanding-brain.s3.us-east-2.amazonaws.com/someimage.png")
+
       eb = ExpandingBrain.generate(brains)
 
-      expect(eb).to be_a_kind_of(String)
-      expect(eb).to match(/https:\/\/expanding-brain.s3.us-east-2.amazonaws.com/)
+      expect(eb).to be_a_kind_of(ExpandingBrain)
+      expect(eb.name).to be_a_kind_of(String)
+      expect(eb.url).to match(/https:\/\/expanding-brain.s3.us-east-2.amazonaws.com/)
     end
   end
 end
